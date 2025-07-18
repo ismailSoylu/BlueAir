71b06ee6e75437758fa7b85e1a3f6df550f8e4b9
ismailSoylu <ismailsoylu0405@gmail.com>
2025-07-18 14:55:25 +0300

    fixed

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     name: "Blue Air",
     displayName: "Blue Air",
     slug: "blueAir",
-    version: "1.0.3",
+    version: "1.0.4",
     orientation: "default",
     icon: "./assets/images/icon.png",
     scheme: "blueair",
@@ -28,7 +28,7 @@ export default {
       },
       edgeToEdgeEnabled: true,
       package: "com.ismailjacob.blueAir",
-      versionCode: 8, // Play Store için sürüm kodunu artırdık
+      versionCode: 9, // Play Store için sürüm kodunu artırdık
       // Uygulama arka planda kalırken state'i koru
       allowBackup: true,
       // Ekran kilitlendiğinde uygulamayı koru
