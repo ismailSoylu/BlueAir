3d420ca49b4cadb2a32f23c6190227bab3593854
ismailSoylu <ismailsoylu0405@gmail.com>
2025-07-18 18:40:27 +0300

    fixed

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     name: "Blue Air",
     displayName: "Blue Air",
     slug: "blueAir",
-    version: "1.0.5",
+    version: "1.0.6",
     orientation: "default",
     icon: "./assets/images/icon.png",
     scheme: "blueair",
@@ -43,7 +43,7 @@ export default {
       },
 
       package: "com.ismailjacob.blueAir",
-      versionCode: 10,
+      versionCode: 11,
 
       allowBackup: true,
       softwareKeyboardLayoutMode: "pan",
