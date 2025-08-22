8a2a7264475f514d13a464a13ea91bbdefdad8a5
ismailSoylu <ismailsoylu0405@gmail.com>
2025-08-22 19:28:25 +0300

    version updated

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     name: "Blue Air",
     displayName: "Blue Air",
     slug: "blueAir",
-    version: "1.1.8",
+    version: "1.1.9",
     orientation: "default",
     icon: "./assets/images/icon.png",
     scheme: "blueair",
@@ -38,7 +38,7 @@ export default {
       // statusBar ve navigationBar ayarları artık react-native-edge-to-edge ile yapılacak
       
       package: "com.ismailjacob.blueAir",
-      versionCode: 24, // Version code artırıldı
+      versionCode: 25, // Version code artırıldı
 
       allowBackup: true,
       softwareKeyboardLayoutMode: "pan",
